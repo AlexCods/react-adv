1ec4a20bbb101cb238c4945e6f89e4971d936354
Alex Alberich <exale@MacBook-Pro-de-Alex-2.local>
2021-11-18 19:59:58 +0100

    LazyLoad basico listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,43 +1,45 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   NavLink,
   Route,
-  Link
 } from "react-router-dom";
 
-import logo from '../logo.svg'
+import logo from "../logo.svg";
 
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { LazyPage1 } from "../01-lazyload/pages";
+import { routes } from "./routes";
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/lazy1" end>Lazy1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" end>Lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" end>Lazy3</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <Suspense fallback={null}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink to={path} end>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/lazy1" element={ <LazyPage1 /> } />
-          <Route path="/lazy2" element={ <LazyPage2 /> } />
-          <Route path="/lazy3" element={ <LazyPage3 /> } />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+
+            <Route path="*" element={<LazyPage1 />} />
+          </Routes>
+        </div>
+      </Router>
+    </Suspense>
   );
-}
+};
